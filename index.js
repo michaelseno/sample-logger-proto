87fd1f7ed7671ef0e99e04192167eb6c974d9fd4
Michael Seno <michaelseno@m800.com>
2018-09-21 16:19:59 +0800

    configured logger

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
-// Main package function
-function isNullOrEmpty(input) {
-    // Returns true if the input is either undefined, null, or empty, false otherwise
-    return (input === undefined || input === null || input === '');
+const fs = require('fs');
+const path = require('path');
+const simpleLogger = require('simple-node-logger');
+
+const logFileLocation = './results/logs/';
+const projectLogFileNameAndType = 'ProjectLog.log';
+
+function createLogFile(platform, logType, testName) {
+  !fs.existsSync(logFileLocation) && fs.mkdirSync(logFileLocation);
+  const date = `_${new Date().toLocaleString()}`;
+  const fileName = `${platform}_${testName.concat(date)}`;
+  const logCatlocation = path.join(__dirname, logFileLocation, fileName);
+  fs.writeFile(logCatlocation, JSON.stringify(logType), (err) => {
+    if (err) {
+      this.projectLog(err, 'error');
+      return;
+    }
+    this.projectLog(`${platform} file created in '/results/logs' folder`);
+  });
 }
 
-function isNotNullOrEmpty(input) {
-    // Returns true if the input is either undefined, null, or empty, false otherwise
-    return (input !== undefined || input !== null || input !== '');
+function projectLog(logMsg, level = 'info') {
+  !fs.existsSync(logFileLocation) && fs.mkdirSync(logFileLocation);
+  const proLoglocation = path.join(__dirname, logFileLocation, projectLogFileNameAndType);
+  const log = simpleLogger.createSimpleLogger(proLoglocation);
+  if (level === 'info') {
+    log.info(logMsg);
+  } else if (level === 'error') {
+    log.error(logMsg);
+  } else if (level === 'warn') {
+    log.warn(logMsg);
+  }
 }
-
-// Make the main function available to other packages that require us
-module.exports = { isNullOrEmpty, isNotNullOrEmpty };
+module.exports = {
+  createLogFile,
+  projectLog,
+};
